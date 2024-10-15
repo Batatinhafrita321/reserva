21233a9f48f6f70d254db3f112d4d9bd4d0bde60
_jackx07 <106999700+Batatinhafrita321@users.noreply.github.com>
2024-10-15 15:13:40 -0300

    Add files via upload

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -4,15 +4,16 @@ async function quantidadeUsuarios() {
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
     const quantidadeUsuarios = Object.values(dados)
-    }
-    
-    quantidadeUsuarios()
     const data = [
         {
-        
         x: nomeDasRedes,
         y: quantidadeUsuarios,
         type: 'bar'
          }
-        ]
-    
+    ]
+    const grafico = document.createElement ('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data)
+}
+quantidadeUsuarios()
